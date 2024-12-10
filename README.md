4476fd9305bc0531a100134a667a2d771dec9be3
Christopher Sargent <christopher.sargent@railhead.io>
2024-12-10 14:09:08 -0500

    Add podman container deploy script CAS

---
+++ README.md
@@ -0,0 +1,16 @@
+![alt text](rh_logo.png)
+  
+#### This repository contains a compose file to deploy awx grafana and prometheus as a container stack for ansible automation platform.
+* 
+
+#### 
+sudo su
+dnf install -y podman-docker podman -y 
+systemctl enable podman
+systemctl start podman
+curl -SL https://github.com/docker/compose/releases/download/v2.28.1/docker-compose-linux-x86_64 -o /usr/sbin/docker-compose
+chmod +x /usr/sbin/docker-compose
+
+
+# 
+
---
+++ deploy_aap_metrics_podman.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Create networks if they don't already exist
+echo "Creating Podman networks..."
+podman network create aap
+podman network create service-mesh
+
+# Create volumes directories and set permissions
+echo "Setting up directories for volumes..."
+mkdir -p ./prometheus_config ./prometheus_storage ./grafana_config ./grafana_storage
+chmod -R 775 ./prometheus_storage ./grafana_storage
+
+# Deploy Prometheus
+echo "Deploying Prometheus..."
+podman run -d \
+  --name tools_prometheus_1 \
+  --hostname prometheus \
+  --restart always \
+  --network aap \
+  -p 9090:9090 \
+  -v "$(pwd)/prometheus_config/prometheus.yml:/etc/prometheus/prometheus.yml:Z" \
+  -v "$(pwd)/prometheus_storage:/prometheus:Z" \
+  prom/prometheus:latest
+
+# Deploy Grafana
+echo "Deploying Grafana..."
+podman run -d \
+  --name tools_grafana_1 \
+  --hostname grafana \
+  --restart always \
+  --network aap \
+  -p 3001:3000 \
+  -v "$(pwd)/grafana_config:/etc/grafana/provisioning:Z" \
+  -v "$(pwd)/grafana_storage:/var/lib/grafana:Z" \
+  grafana/grafana-enterprise:latest
+
+# Check deployments
+echo "Checking deployments..."
+podman ps
+
